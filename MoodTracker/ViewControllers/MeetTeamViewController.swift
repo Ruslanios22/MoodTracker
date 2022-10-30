b43620af35aa73249001f1ec7264d83b9bf0f580
Ruslan Shigapov <ilgamovich@gmail.com>
2022-10-31 01:41:09 +0300

    Additions

---
+++ MoodTracker/ViewControllers/MeetTeamViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MeetTeamViewController.swift
+//  MoodTracker
+//
+//  Created by hiirari on 28.10.2022.
+//
+
+import UIKit
+
+class MeetTeamViewController: UIViewController {
+    
+    @IBOutlet var KatyaPhoto: UIImageView!
+    @IBOutlet var YuriPhoto: UIImageView!
+    @IBOutlet var RuslanPhoto: UIImageView!
+    @IBOutlet var RomanPhoto: UIImageView!
+    @IBOutlet var KaterinaPhoto: UIImageView!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        startButton.layer.cornerRadius = 20
+    }
+    
+    override func viewDidLayoutSubviews() {
+        KatyaPhoto.layer.cornerRadius = KatyaPhoto.frame.width / 2
+        YuriPhoto.layer.cornerRadius = YuriPhoto.frame.width / 2
+        RuslanPhoto.layer.cornerRadius = RuslanPhoto.frame.width / 2
+        RomanPhoto.layer.cornerRadius = RomanPhoto.frame.width / 2
+        KaterinaPhoto.layer.cornerRadius = KaterinaPhoto.frame.width / 2
+    }
+}
